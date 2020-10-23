2c9ee32fc0da49ce771d9a3a1ada0a8e080cf574
Myroslav <94mir.ms@gmail.com>
2020-10-23 04:05:10 +0300

    init project

--- js/helpers/Creational.js
+++
@@ -1,8 +0,0 @@
-export function createElement(element) {
-    return document.createElement(element);
-}
-
-export function appendElement(parent, child) {
-    return parent.appendChild(child);
-}
-
